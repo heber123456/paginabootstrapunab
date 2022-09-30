12aa42394bcaf3d747659cb9e77b10a81f98b404
heber123456 <mr.hean002@gmail.com>
2022-09-30 15:47:31 -0500

    practica

---
+++ js/dinamicas.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.carousel').carousel({
+        interval: 2000
+      })
+
+      $('.mi-popover').popover({
+        container: 'body'
+      })
+
+      $('[data-toggle="tooltip"]').tooltip()
+})
